9176e973ea679b008dad9a7cf6f30921ec9547eb
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-12-03 09:26:17 +0500

    done by @asror

--- main.py
+++ main.py
@@ -34,7 +34,19 @@ class Bored:
         Returns:
             dict: activity data
         '''
-        pass
+        endpoint = "activity/"
+        url = self.url + endpoint
+
+        payload = {
+            'type': type
+        }
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return response.status_code
 
     def get_activity_by_id(self, key: int) -> dict:
         '''get activity by key
@@ -48,7 +60,19 @@ class Bored:
         Returns:
             dict: activity data
         '''
-        pass
+        endpoint = "activity/"
+        url = self.url + endpoint
+
+        payload = {
+            'key': key
+        }
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200: 
+            return response.json()
+        
+        return response.status_code
 
     def get_activity_by_accessibility(self, accessibility: float) -> dict:
         '''get activity by accessibility
@@ -62,8 +86,21 @@ class Bored:
         
         Returns:
             dict: activity data
+            accessibility
         '''
-        pass
+        endpoint = "activity/"
+        url = self.url + endpoint
+
+        payload = {
+            'accessibility': accessibility
+        }
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return response.status_code
 
     def get_activity_by_price(self, price: float) -> dict:
         '''get activity by price
@@ -78,7 +115,19 @@ class Bored:
         Returns:
             dict: activity data
         '''
-        pass
+        endpoint = "activity/"
+        url = self.url + endpoint
+
+        payload = {
+            'price': price
+        }
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return response.status_code
 
     def get_activity_by_price_range(self, minprice: float, maxprice: float) -> dict:
         '''get activity by price
@@ -94,4 +143,17 @@ class Bored:
         Returns:
             dict: activity data
         '''
-        pass
+        endpoint = "activity/"
+        url = self.url + endpoint
+
+        payload = {
+            'minprice': minprice,
+            'maxprice': maxprice
+        }
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code == 200:
+            return response.json()
+        
+        return response.status_code
--- test.py
+++ test.py
@@ -3,4 +3,8 @@ from main import Bored
 
 b = Bored()
 
-print(b.get_activity())
+print(b.get_activity_by_type(type='education'))
+# print(b.get_activity_by_id(1000000))
+# print(b.get_activity_by_accessibility(accessibility=0.7))
+# print(b.get_activity_by_price(price=0.5))
+# print(b.get_activity_by_price_range(minprice=0.2, maxprice=0.9))
